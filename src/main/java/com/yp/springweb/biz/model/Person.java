7744168228733fdb5988d588609c59579bc2d55e
Pomog <thoryur@gmail.com>
2023-06-21 22:52:33 +0300

    thymeleaf example

---
+++ src/main/java/com/yp/springweb/biz/model/Person.java
@@ -0,0 +1,16 @@
+package com.yp.springweb.biz.model;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+public class Person {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private LocalDate dob;
+    private BigDecimal salary;
+}
