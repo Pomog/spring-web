ee796c854c63acd098fac4164c87d1dcb6553640
Pomog <thoryur@gmail.com>
2023-06-25 20:25:50 +0300

    data input validation through annotations

--- src/main/java/com/yp/springweb/biz/model/Person.java
+++ src/main/java/com/yp/springweb/biz/model/Person.java
@@ -2,6 +2,7 @@ package com.yp.springweb.biz.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,9 +18,20 @@ public class Person {
     @Id
     @GeneratedValue
     private Long id;
+
+    @NotEmpty(message = "First name can not be empty")
     private String firstName;
+    @NotEmpty(message = "Last name can not be empty")
     private String lastName;
+    @PastOrPresent(message = "Date od birth must be in the past")
+    @NotNull(message = "Date od birth must be specified")
     private LocalDate dob;
+
+    @DecimalMin(value = "0", message = "Salary must be at least 0")
+    @NotNull(message = "Salary can not be empty")
     private BigDecimal salary;
+
+    @Email(message = "Email must be valid")
+    @NotEmpty(message = "Email can not be empty")
     private String email;
 }
--- src/main/java/com/yp/springweb/web/controller/PeopleController.java
+++ src/main/java/com/yp/springweb/web/controller/PeopleController.java
@@ -2,7 +2,9 @@ package com.yp.springweb.web.controller;
 
 import com.yp.springweb.biz.model.Person;
 import com.yp.springweb.data.PersonRepository;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -32,9 +34,11 @@ public class PeopleController {
     }
 
     @PostMapping
-    public String savePerson(Person person){
-        System.out.println(person);
-        personRepository.save(person);
-        return "redirect:people";
+    public String savePerson(@Valid Person person, Errors errors){
+        if (!errors.hasErrors()) {
+            personRepository.save(person);
+            return "redirect:people";
+        }
+        return "people";
     }
 }
