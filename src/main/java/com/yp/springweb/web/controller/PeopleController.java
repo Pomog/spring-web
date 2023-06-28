b9a5793aff10b4e702f42052578b569fb3a8027e
Pomog <thoryur@gmail.com>
2023-06-28 21:59:42 +0300

    show images using ResponseEntity<Resource>

--- src/main/java/com/yp/springweb/web/controller/PeopleController.java
+++ src/main/java/com/yp/springweb/web/controller/PeopleController.java
@@ -5,6 +5,9 @@ import com.yp.springweb.data.FileStorageRepository;
 import com.yp.springweb.data.PersonRepository;
 import jakarta.validation.Valid;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -15,10 +18,15 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
+import static java.lang.String.format;
+
 @Controller
 @RequestMapping("/people")
 @Log4j2
 public class PeopleController {
+    public static final String DISPOSITION = """
+             attachment; filename="%s"
+            """;
     private PersonRepository personRepository;
     private FileStorageRepository fileStorageRepository;
 
@@ -37,6 +45,15 @@ public class PeopleController {
     @GetMapping
     public String showPeoplePage (){
         return "people";
+    }
+    @GetMapping("/images/{resource}")
+    public ResponseEntity<Resource> getResource(@PathVariable() String resource){
+        System.out.println("!!!!!!!!!!!getResource INVOKED!!!!!!!!!!!!!");
+        log.info("resource : " + resource);
+        return ResponseEntity
+                .ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, format(DISPOSITION, resource))
+                .body(fileStorageRepository.findByName(resource));
     }
     @PostMapping
     public String savePerson(@Valid Person person, Errors errors, @RequestParam("photoFileName") MultipartFile photoFile) throws IOException {
